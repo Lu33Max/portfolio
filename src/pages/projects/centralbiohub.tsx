78d048156755be300a2b536c05380ee4ccba5203
Lu33Max <ernst_lucas@outlook.de>
2024-03-23 15:35:18 +0100

    cbh page fix

---
+++ src/pages/projects/centralbiohub.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function centralbiohub() {
+  return (
+    <>
+			<Head>
+        <title>Ernst</title>
+        <meta name="description" content="Central BioHub" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
